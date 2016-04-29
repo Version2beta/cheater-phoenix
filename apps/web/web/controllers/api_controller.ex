60e037aad5aa0751aff788c75f024e09ca9ffe3a
Rob Martin <rob@version2beta.com>
2016-04-29 14:19:25 +0100

    don't use OTP

--- apps/web/web/controllers/api_controller.ex
+++ apps/web/web/controllers/api_controller.ex
@@ -3,9 +3,8 @@ defmodule Cheater.Web.APIController do
   alias Cheater.Solver
 
   def index(conn, %{"tiles" => tiles}) do
-    {:ok, pid} = Cheater.Solver.Supervisor.start_solver
     dict = ['a', 'ba', 'fa', 'la']
-    suggestions = Cheater.Solver.solve pid, to_char_list(tiles), dict
+    suggestions = Cheater.Solver.suggest to_char_list(tiles), dict
     render conn, suggestions: Enum.map(suggestions, fn suggestion -> to_string suggestion end)
   end
 end
